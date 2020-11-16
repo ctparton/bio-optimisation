aa8f45f5b13c6a582c1a43f8dd5bd226196d71c5
Callum Parton <b7017327@newcastle.ac.uk>
2020-11-16 22:42:09 +0000

    updated csv writer

--- OutputMerger/main.py
+++ OutputMerger/main.py
@@ -3,23 +3,35 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import argparse
+from tqdm import tqdm
 import pandas
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pso', help='add value of true if using pso')
+    args = parser.parse_args()
     REPEATS = 30
-    output_file = "survivors.csv"
+    output_file = "pso_population1m.csv"
     generational_fitness = []
-    generation = 1
+    print(f"Writing to {output_file}")
     with open(pathlib.Path.cwd().parent / 'CW1' / 'out.txt', 'r', encoding="utf-16") as reader:
-        for line in reader:
+        for line in tqdm(reader):
             try:
                 exists = list(filter(lambda g: g['generation'] == int(line.split(" ")[0]), generational_fitness))
                 if len(exists) < 1:
-                    generational_fitness.append(
-                        {'generation': int(line.split(" ")[0]), 'fitness': float(line.split(" ")[1]),
-                         "popSize": line.split(" ")[2], "numSurvivors": line.split(" ")[3],
-                         "tournamentSize": line.split(" ")[4], "probMutation": line.split(" ")[5],
-                         "probCrossover": line.split(" ")[6], "duration": float(line.split(" ")[7])})
+                    if (args.pso):
+                        generational_fitness.append(
+                            {'generation': int(line.split(" ")[0]), 'fitness': float(line.split(" ")[1]),
+                             "numParticles": line.split(" ")[2], "neighWeight": line.split(" ")[3],
+                             "inertiaWeight": line.split(" ")[4], "personalWeight": line.split(" ")[5],
+                             "globalWeight": line.split(" ")[6], "duration": float(line.split(" ")[7])})
+                    else:
+                        generational_fitness.append(
+                            {'generation': int(line.split(" ")[0]), 'fitness': float(line.split(" ")[1]),
+                             "popSize": line.split(" ")[2], "numSurvivors": line.split(" ")[3],
+                             "tournamentSize": line.split(" ")[4], "probMutation": line.split(" ")[5],
+                             "probCrossover": line.split(" ")[6], "duration": float(line.split(" ")[7])})
 
                 else:
                     generational_fitness[generational_fitness.index(exists[0])]['fitness'] = exists[0][
@@ -31,7 +43,7 @@ if __name__ == '__main__':
             except ValueError:
                 pass
 
-    for generation in generational_fitness:
+    for generation in tqdm(generational_fitness):
         generation['fitness'] = generation['fitness'] / REPEATS
         generation['duration'] = generation['duration'] / REPEATS
 
